b925bbb0bfee8facad83d8b8fc4e2e1cc97bd649
mateuszrozek <mateusz.s.rozek@gmail.com>
2020-03-26 17:43:12 +0100

    Initial commit

---
+++ src/test/java/pl/cleanapp/cleanappapi/CleanappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cleanapp.cleanappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
