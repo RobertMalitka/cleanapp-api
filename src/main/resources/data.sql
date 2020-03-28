ce56145c61f1c8f157acbf2a6dfda1d26cbe1f23
mateuszrozek <mateusz.s.rozek@gmail.com>
2020-03-28 12:45:11 +0100

    data inserts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,61 @@
+insert into users values(1, 'Jan');
+insert into users values(2, 'Mateusz');
+insert into users values(3, 'Robert');
+
+insert into weeks values(1, 'CW12');
+insert into weeks values(2, 'CW15');
+insert into weeks values(3, 'CW18');
+insert into weeks values(4, 'CW21');
+insert into weeks values(5, 'CW24');
+insert into weeks values(6, 'CW27');
+insert into weeks values(7, 'CW30');
+insert into weeks values(8, 'CW33');
+insert into weeks values(9, 'CW36');
+insert into weeks values(10, 'CW39');
+insert into weeks values(11, 'CW42');
+insert into weeks values(12, 'CW45');
+insert into weeks values(13, 'CW48');
+
+insert into areas values(1, 'PENDING', 'KCH', 1, 1);
+insert into areas values(2, 'PENDING', 'KOR', 2, 1);
+insert into areas values(3, 'DONE', 'ŁAZ', 3, 1);
+insert into areas values(4, 'INACTIVE', 'ŁAZ', 1, 2);
+insert into areas values(5, 'INACTIVE', 'KCH', 2, 2);
+insert into areas values(6, 'INACTIVE', 'KOR', 3, 2);
+insert into areas values(7, 'INACTIVE', 'KOR', 1, 3);
+insert into areas values(8, 'INACTIVE', 'ŁAZ', 2, 3);
+insert into areas values(9, 'INACTIVE', 'KCH', 3, 3);
+insert into areas values(10, 'INACTIVE', 'KCH', 1, 4);
+insert into areas values(11, 'INACTIVE', 'KOR', 2, 4);
+insert into areas values(12, 'INACTIVE', 'ŁAZ', 3, 4);
+insert into areas values(13, 'INACTIVE', 'ŁAZ', 1, 5);
+insert into areas values(14, 'INACTIVE', 'KCH', 2, 5);
+insert into areas values(15, 'INACTIVE', 'KOR', 3, 5);
+insert into areas values(16, 'INACTIVE', 'KOR', 1, 6);
+insert into areas values(17, 'INACTIVE', 'ŁAZ', 2, 6);
+insert into areas values(18, 'INACTIVE', 'KCH', 3, 6);
+insert into areas values(19, 'INACTIVE', 'KCH', 1, 7);
+insert into areas values(20, 'INACTIVE', 'KOR', 2, 7);
+insert into areas values(21, 'INACTIVE', 'ŁAZ', 3, 7);
+insert into areas values(22, 'INACTIVE', 'ŁAZ', 1, 8);
+insert into areas values(23, 'INACTIVE', 'KCH', 2, 8);
+insert into areas values(24, 'INACTIVE', 'KOR', 3, 8);
+insert into areas values(25, 'INACTIVE', 'KOR', 1, 9);
+insert into areas values(26, 'INACTIVE', 'ŁAZ', 2, 9);
+insert into areas values(27, 'INACTIVE', 'KCH', 3, 9);
+insert into areas values(28, 'INACTIVE', 'KCH', 1, 10);
+insert into areas values(29, 'INACTIVE', 'KOR', 2, 10);
+insert into areas values(30, 'INACTIVE', 'ŁAZ', 3, 10);
+insert into areas values(31, 'INACTIVE', 'ŁAZ', 1, 11);
+insert into areas values(32, 'INACTIVE', 'KCH', 2, 11);
+insert into areas values(33, 'INACTIVE', 'KOR', 3, 11);
+insert into areas values(34, 'INACTIVE', 'KOR', 1, 12);
+insert into areas values(35, 'INACTIVE', 'ŁAZ', 2, 12);
+insert into areas values(36, 'INACTIVE', 'KCH', 3, 12);
+insert into areas values(37, 'INACTIVE', 'KCH', 1, 13);
+insert into areas values(38, 'INACTIVE', 'KOR', 2, 13);
+insert into areas values(39, 'INACTIVE', 'ŁAZ', 3, 13);
+
+
+
+
