5fcb6e126155539cdf21d438567f77846a72127c
Robbur <rmalitka@poczta.onet.pl>
2020-04-06 17:02:35 +0200

    change of AreaStatus, filtering activities

--- src/main/java/pl/cleanapp/cleanappapi/service/impl/AreaServiceImpl.java
+++ src/main/java/pl/cleanapp/cleanappapi/service/impl/AreaServiceImpl.java
@@ -4,15 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.cleanapp.cleanappapi.model.*;
 import pl.cleanapp.cleanappapi.repository.AreaRepository;
+import pl.cleanapp.cleanappapi.service.ActivityService;
 import pl.cleanapp.cleanappapi.service.AreaService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AreaServiceImpl implements AreaService {
 
     @Autowired
     AreaRepository areaRepository;
+
+    @Autowired
+    ActivityService activityService;
+
 
     @Override
     public List<Area> areasByUser(Long userId) {
@@ -27,7 +33,12 @@ public class AreaServiceImpl implements AreaService {
     @Override
     public Area changeAreaStatus(Long areaId) {
         Area area = areaRepository.findById(areaId).get();
-        for (Activity activity : area.getActivities()) {
+        List<Activity> list = area.getActivities().stream()
+                .filter(act -> activityService.checkIfActivityIsNeeded(getWeekByAreaId(areaId),
+                        act.getFrequency()))
+                .collect(Collectors.toList());
+
+        for (Activity activity : list) {
             if (activity.getActivityStatus() != ActivityStatus.CHECKED) {
                 return area;
             }
